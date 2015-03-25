0e6258ef52937e0d8c9f20acbe2106bd1ec31117
Diogo Lopes <diogol_l@hotmail.com>
2015-03-25 12:31:56 -0300

    Mais uma adicao

---
+++ src/Almoxarifado/Codinterno.java
@@ -0,0 +1,27 @@
+package Almoxarifado;
+
+public class Codinterno {
+
+	private int upc;
+	private int receptaculo;
+	private int corredor;
+	
+	public int getUpc() {
+		return upc;
+	}
+	public void setUpc(int upc) {
+		this.upc = upc;
+	}
+	public int getReceptaculo() {
+		return receptaculo;
+	}
+	public void setReceptaculo(int receptaculo) {
+		this.receptaculo = receptaculo;
+	}
+	public int getCorredor() {
+		return corredor;
+	}
+	public void setCorredor(int corredor) {
+		this.corredor = corredor;
+	}
+}
---
+++ src/Almoxarifado/Saida.java
@@ -0,0 +1,16 @@
+package Almoxarifado;
+import java.util.*;
+
+public class Saida {
+
+	private Date datasaida;
+
+	public Date getDatasaida() {
+		return datasaida;
+	}
+
+	public void setDatasaida(Date datasaida) {
+		this.datasaida = datasaida;
+	}
+	
+}
